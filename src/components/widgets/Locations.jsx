3ba08b1a9701c1d434eb6ca8551a9e46717b4de1
grantbirki <grantbirki@github.com>
2023-03-17 14:59:11 +0000

    color card bg

--- src/components/widgets/Locations.jsx
+++ src/components/widgets/Locations.jsx
@@ -3,7 +3,6 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 
 export default function Locations(props) {
   // Dynamically set the gridcss depending on how many items are in the locations list
@@ -16,7 +15,7 @@ export default function Locations(props) {
 
   return (
     <section class="scroll-mt-16">
-      <div class="max-w-6xl mx-auto px-4 sm:px-6">
+      <div class="max-w-6xl mx-auto px-4 sm:px-6 dark:bg-slate-800 bg-blue-50">
         <div class="md:mx-auto text-center">
           <div class="py-12 md:py-20">
             {props.highlight && (
@@ -47,12 +46,8 @@ export default function Locations(props) {
                       image={location.image.src}
                     />
                     <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        {location.name}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        {location.description}
-                      </Typography>
+                      <h3 class="text-4xl md:text-5xl font-bold leading-tighter tracking-tighter mb-4 font-heading">{location.name}</h3>
+                      <p class="max-w-3xl mx-auto sm:text-center text-xl text-muted dark:text-slate-400">{location.description}</p>
                     </CardContent>
                     <CardActions>
                       {location.buttons.map((button) => {
