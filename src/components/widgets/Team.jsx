2e437c6d5c973baa37f6b225ebeb56292c0b886d
grantbirki <grantbirki@github.com>
2023-03-18 16:24:59 +0000

    fix icons

--- src/components/widgets/Team.jsx
+++ src/components/widgets/Team.jsx
@@ -13,12 +13,22 @@ export default function Team(props) {
               {member?.social?.instagram && (
                 <li>
                   <a href={member?.social?.instagram} class="text-[#e4405f] hover:text-gray-900 dark:hover:text-white">
-                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                      <path
-                        fill-rule="evenodd"
-                        d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8zm4.5-11.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm-9 0a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm5.5 6.5c-3.866 0-7-1.343-7-3s3.134-3 7-3 7 1.343 7 3-3.134 3-7 3z"
-                        clip-rule="evenodd"
-                      ></path>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="icon icon-tabler icon-tabler-brand-instagram"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      stroke-width="2"
+                      stroke="currentColor"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                      <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"></path>
+                      <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+                      <path d="M16.5 7.5l0 .01"></path>
                     </svg>
                   </a>
                 </li>
@@ -48,12 +58,24 @@ export default function Team(props) {
               {member?.social?.linkedin && (
                 <li>
                   <a href={member?.social?.linkedin} class="text-[#0077b5] hover:text-gray-900 dark:hover:text-white">
-                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                      <path
-                        fill-rule="evenodd"
-                        d="M4 6.75C4 5.784 4.784 5 5.75 5h12.5c.966 0 1.75.784 1.75 1.75v10.5c0 .966-.784 1.75-1.75 1.75H5.75A1.75 1.75 0 014 17.25V6.75zm5.75-.25a.25.25 0 00-.25.25v10.5a.25.25 0 00.25.25h12.5a.25.25 0 00.25-.25V6.75a.25.25 0 00-.25-.25H9.75zM6.5 8a1.5 1.5 0 100 3 1.5 1.5 0 000-3zm0 1a.5.5 0 110 1 .5.5 0 010-1zm0 4.25a.75.75 0 100"
-                        clip-rule="evenodd"
-                      ></path>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="icon icon-tabler icon-tabler-brand-linkedin"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      stroke-width="2"
+                      stroke="currentColor"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                      <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
+                      <path d="M8 11l0 5"></path>
+                      <path d="M8 8l0 .01"></path>
+                      <path d="M12 16l0 -5"></path>
+                      <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
                     </svg>
                   </a>
                 </li>
