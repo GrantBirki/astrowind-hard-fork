b933b0b54337888d99c3a860f0d1bb9b303e79ad
grantbirki <grantbirki@github.com>
2023-03-18 15:50:18 +0000

    init teams component in react

---
+++ src/config/components/team.js
@@ -0,0 +1,32 @@
+// Configuration for the team component
+
+const team = {
+  enabled: true,
+
+  // The title of the team section
+  title: 'Our Team',
+
+  // The subtitle of the team section
+  description: 'Meet the team behind this project',
+
+  // Members of the team
+  members: [
+    {
+      // The name of the first member
+      name: 'Neil Sims',
+      // The job title of the first member
+      title: 'Vue.js Developer',
+      // The avatar of the first member
+      avatar: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png',
+      // Social links of the first member
+      social: {
+        twitter: '#',
+        facebook: '#',
+        linkedin: '#',
+        github: '#',
+      },
+    },
+  ],
+};
+
+export default team;
