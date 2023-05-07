1cb329869cdc1de32d3afd7de1d3b94d9af4bf30
grantbirki <grantbirki@github.com>
2023-05-07 11:25:34 +0100

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { defineConfig, sharpImageService } from "astro/config";
+import { defineConfig, sharpImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
