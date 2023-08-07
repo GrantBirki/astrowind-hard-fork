42e96c84cdd1c47f386a0972ad5bdc3352c2c3e0
grantbirki <grantbirki@github.com>
2023-08-07 16:23:15 +0100

    wow

--- src/config/site/blog.js
+++ src/config/site/blog.js
@@ -1,31 +1,43 @@
 // blog post configuration
 
 const blog = {
-  disabled: false,
+  isEnabled: true,
   postsPerPage: 4,
 
   post: {
     permalink: '/%slug%', // Variables: %slug%, %year%, %month%, %day%, %hour%, %minute%, %second%, %category%
     noindex: false,
-    disabled: false,
+    isEnabled: true,
+    robots: {
+      index: true,
+    },
   },
 
   list: {
     pathname: 'blog', // Blog main path, you can change this to "articles" (/articles)
     noindex: false,
-    disabled: false,
+    isEnabled: true,
+    robots: {
+      index: true,
+    },
   },
 
   category: {
     pathname: 'category', // Category main path /category/some-category
     noindex: true,
-    disabled: false,
+    isEnabled: true,
+    robots: {
+      index: true,
+    },
   },
 
   tag: {
     pathname: 'tag', // Tag main path /tag/some-tag
     noindex: true,
-    disabled: false,
+    isEnabled: true,
+    robots: {
+      index: false,
+    },
   },
 };
 
